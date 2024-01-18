631cdf6b22293659f20308576d819483ccad31b8
VilleK88 <ville.karppinen88@gmail.com>
2024-01-18 12:38:38 +0200

    Save & load system improvements
    Collected coins now clear from the cashIDs array when starting new game.

--- Assets/Scripts/UI/MenuControls.cs
+++ Assets/Scripts/UI/MenuControls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,7 @@ public class MenuControls : MonoBehaviour
 
     public void StartGame()
     {
+        GameManager.instance.cashIDs = new int[0]; // clear collected currency IDs
         SceneManager.LoadScene("TestScene");
     }
 
