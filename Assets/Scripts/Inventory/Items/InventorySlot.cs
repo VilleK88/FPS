65f8ed290b80f7bb987de66e3408a1744e7a59be
VilleK88 <126750132+VilleK88@users.noreply.github.com>
2024-03-25 12:03:50 +0200

    Upgrading inventory

--- Assets/Scripts/Inventory/Items/InventorySlot.cs
+++ Assets/Scripts/Inventory/Items/InventorySlot.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour, IPointerClickHandler
 {
     public Item item;
     public int itemId = -1;
@@ -15,6 +16,8 @@ public class InventorySlot : MonoBehaviour
     public int maxStack;
     public int count;
     TextMeshProUGUI countText;
+
+    bool dragging = false;
 
     private void Start()
     {
@@ -27,5 +30,54 @@ public class InventorySlot : MonoBehaviour
         bool textActive = count > 1;
         if(textActive)
             countText.text = count.ToString();
+        else
+        {
+            countText.text = "";
+        }
+    }
+
+    public void UseItem()
+    {
+        if(item != null)
+        {
+            if(!dragging)
+            {
+                item.Use();
+                if(stackable)
+                {
+                    count--;
+                    RefreshCount();
+                    DestroyItem();
+                }
+                else
+                {
+                    count--;
+                    DestroyItem();
+                }
+            }
+        }
+    }
+
+    void DestroyItem()
+    {
+        if (count <= 0)
+        {
+            item = null;
+            itemId = -1;
+            itemName = null;
+            maxStack = 0;
+            count = 0;
+            transform.GetChild(0).GetComponentInChildren<Image>().sprite = null;
+            transform.GetChild(0).GetComponentInChildren<Image>().color = new Color(0, 0, 0, 0);
+            itemType = ItemType.Default;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            UseItem();
+        }
     }
 }
