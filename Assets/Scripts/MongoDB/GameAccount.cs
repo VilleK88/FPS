f345ced528901522d753f1f1a778d06ec67eba4a
VilleK88 <ville.karppinen88@gmail.com>
2024-05-31 10:16:18 +0300

    Login in progress...

--- Assets/Scripts/MongoDB/GameAccount.cs
+++ Assets/Scripts/MongoDB/GameAccount.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
+[System.Serializable]
 public class GameAccount
 {
     public string _id;
     public int adminFlag;
     public string username;
-    public string characterName;
 }
