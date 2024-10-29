74deb37491c46519503e19e05b169baa735071b1
VilleK88 <126750132+VilleK88@users.noreply.github.com>
2024-10-29 15:17:22 +0200

    Multisave system in progress...

--- Assets/Scripts/UI/SavePrefab.cs
+++ Assets/Scripts/UI/SavePrefab.cs
@@ -1,10 +1,44 @@
+using System.Globalization;
+using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
-public class SavePrefab : MonoBehaviour
+public class SavePrefab : MonoBehaviour, IPointerClickHandler
 {
+    public Button myButton;
     public Image saveImage;
     public TextMeshProUGUI saveName;
     public TextMeshProUGUI timeDate;
-    //public GameData gameData;
+    public GameObject removeBG;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("Oikeaa hiiren näppäintä painettu");
+            removeBG.SetActive(true);
+        }
+    }
+    public void RemoveSave()
+    {
+        string fileName = saveName.text;
+        Debug.Log($"Savename: {fileName}");
+        if (!fileName.EndsWith(".dat"))
+            fileName += ".dat";
+        string saveFilePath = Path.Combine(Application.persistentDataPath, fileName);
+        string screenshotPath = Path.Combine(Application.persistentDataPath, fileName.Replace(".dat", ".png"));
+        if (File.Exists(saveFilePath))
+            File.Delete(saveFilePath);
+        else
+            Debug.Log("Tiedostoa ei löytynyt");
+        if (File.Exists(screenshotPath))
+            File.Delete(screenshotPath);
+        else
+            Debug.Log("Tiedostoa ei löytynyt");
+        Destroy(gameObject);
+    }
+    public void CloseRemoveMenu()
+    {
+        removeBG.SetActive(false);
+    }
 }
